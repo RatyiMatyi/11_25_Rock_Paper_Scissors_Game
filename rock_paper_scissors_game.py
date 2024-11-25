5a1b9f79bc9c529b294d52f48d8953e557cfdb56
RatyiMatyi <barta.bulcsu@diak.szbi-pg.hu>
2024-11-25 09:38:35 +0100

    dd
    d

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -1,5 +1,41 @@
 import random
 
-choices = ["rock", "paper", "scissors"]
+def jatek():
+    print("Üdvözölek a Kő, Papír, Olló játékban!")
 
-# machine_choice = random.choice(choices)
+
+    győzelmek = 0
+    vesztések = 0
+    döntetlenek = 0
+
+    while True:
+
+        valasztas = input("Válasz karaktert: Kő, Papír, Olló -> ").strip().lower()
+
+        if valasztas not in ["kő", "papír", "olló"]:
+            print("Érvénytelen választás! Kérlek, válassz a 'kő', 'papír' vagy 'olló' közül.")
+            continue
+
+        gep_valasztasa = random.choice(["kő", "papír", "olló"])
+        print(f"Gép választása: {gep_valasztasa}")
+
+        if valasztas == gep_valasztasa:
+            print("Döntetlen!")
+            döntetlenek += 1
+        elif (valasztas == "kő" and gep_valasztasa == "olló") or \
+             (valasztas == "papír" and gep_valasztasa == "kő") or \
+             (valasztas == "olló" and gep_valasztasa == "papír"):
+            print("Nyertél!")
+            győzelmek += 1
+        else:
+            print("Vesztettél!")
+            vesztések += 1
+
+        print(f"nyerések: {győzelmek}, vesztések: {vesztések}, döntetlenek: {döntetlenek}")
+
+        válasz = input("Akarsz még játszani? (i/n) ").strip().lower()
+        if válasz != 'i':
+            print(f"Köszönjük a játékot! A végső eredmény: {győzelmek} győzelem, {vesztések} vereség, {döntetlenek} döntetlen.")
+            break
+
+jatek()
